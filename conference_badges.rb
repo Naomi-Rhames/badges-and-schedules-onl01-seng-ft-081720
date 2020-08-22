2169b6a746267fb4508bb3bf5530ca294a774833
binding-proc-8938 <naomirhames2020@gmail.com>
2020-08-22 00:13:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ end
 def assign_rooms(speakers)
   new_array=[]
   speakers.each_with_in, idex{|name, i| 
-  hew_array << "Hello, #{name}! You'll be assigned  to room #{i + 1}!"
+    new_array << "Hello, #{name}! You'll be assigned  to room #{i + 1}!"
    end
  new_array 
  end
